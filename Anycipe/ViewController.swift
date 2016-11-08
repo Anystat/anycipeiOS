a5a1f4d9e104d1baca69f1bde9efa09518a4f564
kurotkin <kurotkin@gmail.com>
2016-11-08 16:51:45 +0300

    Add colors

--- Anycipe/ViewController.swift
+++ Anycipe/ViewController.swift
@@ -7,12 +7,13 @@
 //
 
 import UIKit
+import ChameleonFramework
 
-class ViewController: UIViewController {
+@IBDesignable class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        view.backgroundColor = UIColor.red
     }
 
     override func didReceiveMemoryWarning() {
